46a1751ffc6b5512c1aff9c4a839b2cab442dc4a
Iftikhar uddin <iftikhar@connextar.com>
2023-10-09 21:50:08 +0500

    Updated services component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 import Image from 'next/image';
-import {Navbar, Footer, HeroSection} from "../components/components-index";
+import {Navbar, Footer, HeroSection, Service} from "../components/components-index";
 
 const MyApp = ({Component, pageProps}) => (
     <div>
         <Navbar/>
         <HeroSection/>
+        <Service/>
         <Footer/>
     </div>
 );
--- components/components-index.js
+++ components/components-index.js
@@ -3,4 +3,6 @@ import Button from "./Button/button";
 import Footer from "./Footer/footer";
 import Navbar from "./NavBar/navbar";
 import HeroSection from "./HeroSection/HeroSection";
-export {Button, Footer, Navbar, HeroSection};
+import Service from "./Service/Service";
+
+export {Button, Footer, Navbar, HeroSection, Service};
