ff02de5357faf7bc4c2525965e50f1d1a2f45617
Iftikhar uddin <iftikhar@connextar.com>
2023-10-10 14:31:28 +0500

    Author page added

--- authorPage/componentIndex.js
+++ authorPage/componentIndex.js
@@ -1,5 +1,5 @@
 import AuthorProfileCard from "./AuthorProfileCard/AuthorProfileCard";
 import AuthorTaps from "./AuthorTaps/AuthorTaps";
-import AuthorNFTCardBox from "./AuthorNFTCardBox/AuthorNFTCarBox";
+import AuthorNFTCardBox from "./AuthorNFTCardBox/AuthorNFTCardBox";
 
 export { AuthorProfileCard, AuthorTaps, AuthorNFTCardBox };
