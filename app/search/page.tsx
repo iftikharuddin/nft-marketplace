77c5e2436d6c7fc64b046a1adc87261230de6e99
Iftikhar uddin <iftipro@gmail.com>
2023-10-10 14:52:54 +0500

    search bar added

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React from "react";
 
-function Search(props) {
+//INTRNAL IMPORT
+import Style from "../styles/searchPage.module.css";
+import { Slider, Brand } from "../../components/components-index";
+import { SearchBar } from "../../SearchPage/searchPageIndex";
+import { Filter } from "../../components/components-index";
+
+import { NFTCardTwo, Banner } from "../../collectionPage/collectionIndex";
+import images from "../../img";
+
+const searchPage = () => {
+    const collectionArray = [
+        images.nft_image_1,
+        images.nft_image_2,
+        images.nft_image_3,
+        images.nft_image_1,
+        images.nft_image_2,
+        images.nft_image_3,
+        images.nft_image_1,
+        images.nft_image_2,
+    ];
     return (
-        <div> yo searchy</div>
+        <div className={Style.searchPage}>
+            <Banner bannerImage={images.creatorbackground5} />
+            <SearchBar />
+            <Filter />
+            <NFTCardTwo NFTData={collectionArray} />
+            <Slider />
+            <Brand />
+        </div>
     );
-}
+};
 
-export default Search;
+export default searchPage;
